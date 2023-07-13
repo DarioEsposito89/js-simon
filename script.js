0f6416299b13ae02a4ef884bc18eafb26ce072b1
DarioEsposito89 <darioesposito.work@gmail.com>
2023-07-13 15:06:51 +0200

    esercizio completo

--- script.js
+++ script.js
@@ -4,3 +4,33 @@
 // ----------------------------------------------------------------------------------------------
 // Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
 // Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
+
+// Inserisco la data e l'ora della fine del countdown
+const countDownDate = new Date("Jul 14, 2023 09:30:00").getTime();
+
+// Aggiorno il conteggio ogni secondo
+let x = setInterval(function() {
+
+  // Mi procuro la data aggiornata di oggi
+    let liveDate = new Date().getTime();
+    
+  // mi procuro il divario tra data di oggi e data di arrivo
+    let difference = countDownDate - liveDate;
+    
+  // Tempo calcolato per giorni, ore, minuti and secondi
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    
+  // stampo il countdown in tempo reale sullo schermo
+    document.getElementById("liveTime").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+  // Se il countdown è finito, si scrive questo
+    if (difference < 0) {
+    clearInterval(x);
+    document.getElementById("liveTime").innerHTML = "Inizio lezione";
+    document.getElementById("title").innerHTML = "";
+    }
+}, 1000);
